c264e539cef41dd0d2bc9782be86116be81d6f4f
Amonel Leais <manoelaraujo24@gmail.com>
2023-11-25 10:57:48 -0300

    Alteração no modelo das rotas
    Alterei o modelo das rotas, que antes estava centrado na api.js para um router com as rotas separadas.

--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 
-router.get('/users', userController.getAll);
-router.post('/users', userController.create);
+router.route('/').post((req, res) => userController.create(req, res));
+router.route('/').get((req, res) => userController.getAll(req, res));
 
 module.exports = router;
