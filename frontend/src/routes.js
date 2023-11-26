da879eb8796a3b5e4d1789368afd5b926ca138d7
iamgabs <anjomito26@gmail.com>
2023-11-26 20:38:15 -0300

    feat: fix and feature
    ajustei a estrutura novamente, fiz o header e footer

---
+++ frontend/src/routes.js
@@ -0,0 +1,29 @@
+import React from "react";
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+  } from "react-router-dom";
+  
+import Login from './pages/Login';
+// import Home from './pages/Home';
+import CadastroDeProdutos from './pages/CadastroDeProdutos';
+import CadastroDeUsuarios from './pages/CadastroDeUsuarios';
+import ListagemDeProdutos from './pages/ListagemDeProdutos';
+import ListagemDeUsuarios from './pages/ListagemDeUsuarios';
+
+
+export default function WebRoute() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Login />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/cadastrar-produtos" element={<CadastroDeProdutos />} />
+                <Route path="/cadastrar-usuario" element={<CadastroDeUsuarios />} />
+                <Route path="/listar-usuarios" element={<ListagemDeUsuarios />} />
+                <Route path="/listar-produtos" element={<ListagemDeProdutos />} />
+            </Routes>
+        </BrowserRouter>
+    );
+}
