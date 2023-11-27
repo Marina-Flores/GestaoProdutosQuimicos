934923163958e8be9201fe8ac53de5bad0e0e3a1
Luisa Ferreira <49447760+luisaferreira@users.noreply.github.com>
2023-11-27 14:53:41 -0300

    Implementada a tela de listagem de usuários

---
+++ frontend/src/components/ListaUsuarios/ListaUsuarios.jsx
@@ -0,0 +1,100 @@
+import React, { useState } from 'react';
+import './listaUsuarios.css';
+import Header from '../Header/Header';
+
+export default function ListaUsuarios() {
+    const [usuarios, setUsuarios] = useState([
+        { Nome: "Maria Luisa Ferreira", Cargo: "Analista", Ativo: true },
+        { Nome: "Mike Kennedy", Cargo: "Analista", Ativo: false },
+        { Nome: "Gabriel Carvalho", Cargo: "Analista", Ativo: true },
+        { Nome: "Lucas Santos", Cargo: "Professor", Ativo: true },
+        { Nome: "Ester Vieira", Cargo: "Analista", Ativo: true },
+        { Nome: "Marina Flores", Cargo: "Professor", Ativo: false },
+        { Nome: "Manoel Elias", Cargo: "Analista", Ativo: false },
+    ]);
+    return (
+        <div>
+            <Header />
+            <div className='listar-usuarios'>
+                <h1 className='titulo'>Listagem de Usuários</h1>
+
+                <div className='container-principal'>
+                    <div className='container-filtros'>
+                        <aside className='filtros-usuario'>
+                            <h2 className='titulo-filtro'>Filtros</h2>
+                            <div className='container-filtro-secao'>
+                                <h4 className='titulo-filtro-secao'>Status</h4>
+                                <div className='container-filtro-secao-opcoes'>
+                                    <label className='pointer'><input type='checkbox'></input> Ativo</label>
+                                    <label className='pointer'><input type='checkbox'></input> Inativo</label>
+                                </div>
+                            </div>
+                            <div className='container-filtro-secao'>
+                                <h4 className='titulo-filtro-secao'>Cargo</h4>
+                                <div className='container-filtro-secao-opcoes'>
+                                    <label className='pointer'><input type='checkbox'></input> Professor</label>
+                                    <label className='pointer'><input type='checkbox'></input> Analista</label>
+                                </div>
+                            </div>
+                        </aside>
+                    </div>
+
+
+                    <div className='container-usuarios'>
+                        <div className='container-opcoes-usuarios'>
+                            <input type='text' placeholder='Buscar' className='input-busca'></input>
+                            <i class="fa-solid fa-magnifying-glass icone-barra-pesquisa"></i>
+                            <div className='container-opcoes-usuarios-botoes'>
+                                <select className='select-quantidade-itens pointer'>
+                                    <option value={10} selected>10 usuários por página</option>
+                                    <option value={20}>20 usuários por página</option>
+                                    <option value={50}>50 usuários por página</option>
+                                </select>
+                                <button className='btn-novo-usuario pointer'>Novo usuário <i class="fa-solid fa-plus"></i></button>
+                            </div>
+                        </div>
+                        <div className='container-lista-cabecalho'>
+                            <h5 className='titulo-lista-cabecalho-nome'>Nome</h5>
+                            <h5 className='titulo-lista-cabecalho'>Cargo</h5>
+                            <h5 className='titulo-lista-cabecalho'>Status</h5>
+                            <h5 className='titulo-lista-cabecalho-acoes'>Ações</h5>
+                        </div>
+
+                        <ul className='lista-usuarios'>
+                            {
+                                usuarios.map(usuario => (
+                                    <li className='item-usuario'>
+                                        <p className='item-usuario-nome'>{usuario.Nome}</p>
+                                        <p className='item-usuario-cargo'>{usuario.Cargo}</p>
+                                        {usuario.Ativo ? (
+                                            <p className='item-usuario-status'>Ativo</p>
+                                        ) : (
+                                            <p className='item-usuario-status'>Inativo</p>
+                                        )}
+                                        <div className='item-usuario-acoes'>
+                                            <a className='pointer'>
+                                                <i class="fa-regular fa-pen-to-square icone icone-edicao"></i>
+                                            </a>
+                                            <a className='pointer'>
+                                                <i class="fa-solid fa-trash icone icone-delecao"></i>
+                                            </a>
+                                        </div>
+                                    </li>
+                                ))
+                            }
+                        </ul>
+
+                        <div className='botoes-paginacao'>
+                            <button className='pagina-anterior pointer'>
+                                <i class="fa-solid fa-chevron-left"></i> Anterior
+                            </button>
+                            <button className='proxima-pagina pointer'>
+                                Próxima <i class="fa-solid fa-chevron-right"></i>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
