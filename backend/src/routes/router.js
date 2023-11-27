6978b9ebf369ef39c9f1f29b3db0a8f47ef2b190
Amonel Leais <manoelaraujo24@gmail.com>
2023-11-27 09:06:31 -0300

    Criação da Tabela de Logs de Produtos

--- backend/src/routes/router.js
+++ backend/src/routes/router.js
@@ -3,9 +3,11 @@ const router = require('express').Router()
 const usersRouter = require('./users');
 const produtosRouter = require('./produtos');
 const loginRouter = require('./auth');
+const logProdutosRouter = require('./logprodutos');
 
 router.use('/users', usersRouter);
 router.use('/produtos', produtosRouter);
 router.use('/login', loginRouter);
+router.use('/log/produto', logProdutosRouter);
 
 module.exports = router;
