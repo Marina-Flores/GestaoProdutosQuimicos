a3dca986e82fb170fd74a5803bcf3cd7d8c3b995
Amonel Leais <manoelaraujo24@gmail.com>
2023-11-28 09:20:58 -0300

    Alteração na lógica do Update

--- backend/src/controllers/produtoController.js
+++ backend/src/controllers/produtoController.js
@@ -9,7 +9,6 @@ const produtoController = {
             const logProduto = new LogProduto({
                 produto_id: newProduto._id,
                 user_id: userId,
-                qtd_utilizada: req.body.qtd_utilizada,
                 descricao: `Produto '${req.body.nome}' adicionado pelo Usuário '${userId}'`,
             })
             await logProduto.save();
@@ -38,16 +37,17 @@ const produtoController = {
     update: async (req, res) => {
         try{
             const id = req.params.id;
+            const produto = await Produto.findOne({_id: id});
             const userId = req.user._id;
             const logProduto = new LogProduto({
                 produto_id: id,
                 user_id: userId,
-                qtd_utilizada: req.body.qtd_utilizada,
+                qtd_utilizada: produto.quantidade - req.body.quantidade,
                 descricao: `Produto '${req.body.nome}' alterado pelo Usuário '${userId}'`,
             })
             await logProduto.save();
-            const produto = await Produto.updateOne({_id: id}, {$set: req.body});
-            res.status(200).json(produto);
+            const atualizar = await Produto.updateOne({_id: id}, {$set: req.body});
+            res.status(200).json(atualizar);
         }catch(error) {
             res.status(500).json({error: error.message});
         }
@@ -60,7 +60,6 @@ const produtoController = {
             const logProduto = new LogProduto({
                 produto_id: id,
                 user_id: userId,
-                qtd_utilizada: req.body.qtd_utilizada,
                 descricao: `Produto '${produto.nome}' excluído pelo Usuário '${userId}'`,
             })
             await logProduto.save();
