923f015aac684439c666d2ccf272674243921900
Amonel Leais <manoelaraujo24@gmail.com>
2023-11-29 08:34:53 -0300

    Criação da Tabela de Log do Usuário
    inclusão da rota de log no router e pequenos ajustes para incluir o token nos endpoints

--- backend/src/routes/router.js
+++ backend/src/routes/router.js
@@ -4,11 +4,13 @@ const usersRouter = require('./users');
 const produtosRouter = require('./produtos');
 const loginRouter = require('./auth');
 const logProdutosRouter = require('./logprodutos');
+const logUsersRouter = require('./logusers');
 const verifyToken = require('../middlewares/authMiddleware');
 
-router.use('/users', usersRouter);
+router.use('/users', verifyToken, usersRouter);
 router.use('/produtos', verifyToken, produtosRouter);
 router.use('/login', loginRouter);
-router.use('/log/produto', logProdutosRouter);
+router.use('/log/produtos', verifyToken, logProdutosRouter);
+router.use('/log/users', verifyToken, logUsersRouter);
 
 module.exports = router;
