f44e6734c8bac6f90fde6a5fb3a312da3dbc981f
Luisa Ferreira <49447760+luisaferreira@users.noreply.github.com>
2023-11-29 11:48:50 -0300

    Implementada a validação de campos na tela de edição de usuários

--- frontend/src/components/EditarUsuario/EditarUsuario.jsx
+++ frontend/src/components/EditarUsuario/EditarUsuario.jsx
@@ -1,31 +1,73 @@
+import React, { useEffect } from 'react';
+import Header from '../Header/Header';
 import './editarUsuario.css';
 
-export default function EditarUsuario() {
+const EditarUsuario = () => {
+    useEffect(() => {
+        var btnSalvar = document.getElementById('btn-salvar');
+        btnSalvar.onclick = function () {
+            var contador = 0;
+            var nome = document.getElementById('nome-usuario');
+            var matricula = document.getElementById('matricula-usuario');
+            var cargo = document.getElementById('cargo-usuario');
+            var nomeLabel = document.getElementById('label-nome-usuario');
+            var matriculaLabel = document.getElementById('label-matricula-usuario');
+            var cargoLabel = document.getElementById('label-cargo-usuario');
+            var nomeRegex = /^[a-zA-Z\s]+$/;
+
+            if (nome.value !== "" && nomeRegex.test(nome.value)) {
+                contador++;
+                nomeLabel.classList.remove('label-erro');
+            } else {
+                nomeLabel.classList.add('label-erro');
+            }
+
+            if (matricula.value !== "") {
+                contador++;
+                matriculaLabel.classList.remove('label-erro');
+            } else {
+                matriculaLabel.classList.add('label-erro');
+            }
+            if (cargo.value !== "") {
+                contador++;
+                cargoLabel.classList.remove('label-erro');
+            } else {
+                cargoLabel.classList.add('label-erro');
+            }
+
+            if(contador == 3) {
+                // Fazer a lógica de edição de usuário
+            }
+        }
+    })
+
     return (
         <div>
-            {/* <Header /> */}
+            <Header />
             <div className='container-cadastro'>
                 <h1 className='titulo'>Editar Usuário</h1>
 
                 <div className='container-input'>
-                    <label className='label'>Nome</label>
-                    <input type="text" className='input'></input>
+                    <label className='label' for='nome-usuario' id='label-nome-usuario'>Nome</label>
+                    <input type="text" className='input' id="nome-usuario"></input>
                 </div>
                 <div className='container-input'>
-                    <label className='label'>Matrícula</label>
-                    <input type="text" className='input'></input>
+                    <label className='label' for='matricula-usuario' id='label-matricula-usuario'>Matrícula</label>
+                    <input type="text" className='input' id='matricula-usuario'></input>
                 </div>
                 <div className='container-input'>
-                    <label className='label'>Cargo</label>
-                    <select className='input'>
+                    <label className='label' for='cargo-usuario' id='label-cargo-usuario'>Cargo</label>
+                    <select className='input' id='cargo-usuario'>
                         <option></option>
-                        <option>Analista</option>
-                        <option>Professor</option>
+                        <option value={"Analista"}>Analista</option>
+                        <option value={"Professor"}>Professor</option>
                     </select>
                 </div>
 
-                <button className='btn-salvar pointer'>Salvar</button>
+                <button className='btn-salvar pointer' id='btn-salvar'>Salvar</button>
             </div>
         </div>
     );
 }
+
+export default EditarUsuario;
