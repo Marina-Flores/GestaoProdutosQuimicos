1cf30e5a178507716f98f0e3573a98fc7cbdda9d
Amonel Leais <manoelaraujo24@gmail.com>
2023-11-29 08:40:57 -0300

    Alteração no código de criação de Log de Produto
    pequena alteração na criação dos logs para seguir a consistência na criação de documentos

--- backend/src/controllers/produtoController.js
+++ backend/src/controllers/produtoController.js
@@ -6,12 +6,11 @@ const produtoController = {
         try{
             const newProduto = await Produto.create(req.body);
             const userId = req.user._id;
-            const logProduto = new LogProduto({
+            const logProduto = await LogProduto.create({
                 produto_id: newProduto._id,
                 user_id: userId,
                 descricao: `Produto '${req.body.nome}' adicionado pelo Usuário '${userId}'`,
-            })
-            await logProduto.save();
+            });
             res.status(201).json(newProduto);
         } catch(error) {
             res.status(500).json({error: error.message});
@@ -39,13 +38,12 @@ const produtoController = {
             const id = req.params.id;
             const produto = await Produto.findOne({_id: id});
             const userId = req.user._id;
-            const logProduto = new LogProduto({
+            const logProduto = await LogProduto.create({
                 produto_id: id,
                 user_id: userId,
                 qtd_utilizada: produto.quantidade - req.body.quantidade,
                 descricao: `Produto '${req.body.nome}' alterado pelo Usuário '${userId}'`,
-            })
-            await logProduto.save();
+            });
             const atualizar = await Produto.updateOne({_id: id}, {$set: req.body});
             res.status(200).json(atualizar);
         }catch(error) {
@@ -57,12 +55,11 @@ const produtoController = {
             const id = req.params.id;
             const userId = req.user._id;
             const produto = await Produto.findOne({_id: id});
-            const logProduto = new LogProduto({
+            const logProduto = await LogProduto.create({
                 produto_id: id,
                 user_id: userId,
                 descricao: `Produto '${produto.nome}' excluído pelo Usuário '${userId}'`,
-            })
-            await logProduto.save();
+            });
             const excluir = await Produto.deleteOne({_id: id});
             res.status(200).json(excluir);
         }catch(error){
