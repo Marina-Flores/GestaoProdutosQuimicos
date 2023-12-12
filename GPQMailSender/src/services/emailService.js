2535e752e52591b827924918ee6bfd3503412735
Marina Flores <mflores@ionic.health>
2023-12-12 06:22:09 -0300

    fix: Correção na chamada da APIKey do Sendgrid.

--- GPQMailSender/src/services/emailService.js
+++ GPQMailSender/src/services/emailService.js
@@ -1,34 +1,27 @@
 const sgMail = require('@sendgrid/mail');
+require('dotenv').config();
+
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const enviarEmail = async (destinatario) => {
-  const corpoTexto = `Olá!\n\nEste é um e-mail de exemplo em texto simples.`;
 
-  const corpoHTML = `
-    <html>
-      <body>
-        <h1>Olá!</h1>
-        <p>Este é um e-mail de exemplo em HTML.</p>
-      </body>
-    </html>
-  `;
+    const msg = {
+        to: destinatario,
+        from: 'mlbtf@discente.ifpe.edu.br',
+        templateId: 'd-a1ada49d3f59433998f5613494237ac2',
+        dynamicTemplateData: {
+            urlBotao: 'https://localhost:3000/',
+        }
+    };
 
-  const msg = {
-    to: destinatario,
-    from: 'mlbtf@discente.ifpe.edu.br',
-    subject: 'Esqueci Minha Senha',
-    text: corpoTexto, // Corpo de texto simples
-    html: corpoHTML, // Corpo em HTML
-  };
-
-  try {
-    await sgMail.send(msg);
-    console.log('E-mail enviado com sucesso!');
-    return true;
-  } catch (error) {
-    console.error('Erro ao enviar e-mail:', error);
-    return false;
-  }
+    try {
+        await sgMail.send(msg);
+        console.log('E-mail enviado com sucesso!');
+        return true;
+    } catch (error) {
+        console.error('Erro ao enviar e-mail:', error);
+        return false;
+    }
 };
 
 module.exports = { enviarEmail };
